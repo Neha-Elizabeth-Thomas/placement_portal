1a42cbdb0045a3d0e09ce15df2f9d61b52f397d1
Neha Elizabeth Thomas <nehaelizabeththomas0@gmail.com>
2025-03-09 15:36:32 +0530

    added

---
+++ Backend_server/src/db.js
@@ -0,0 +1,27 @@
+import pg from "pg";
+
+const db = new pg.Pool({
+    user: "postgres",
+    host: "localhost",
+    database: "placement_app",
+    password: "neha2004",
+    port: 5432,
+});
+
+/*
+const db = new pg.Pool({
+    user: "postgres",
+    host: "localhost",
+    database: "placement_app",
+    password: "neha2004",
+    port: 5432,
+});
+*/
+
+// Handling unexpected errors
+db.on("error", (err) => {
+    console.error("Unexpected error on idle client", err);
+});
+
+// Query function using the pool
+export const query = (text, params) => db.query(text, params);
